4febd7b8029e2c73d5af387749acd005aeef3c78
Emilia <emilia3d.pulawy@gmail.com>
2026-01-03 22:02:55 +0100

    3.2

--- Matura_Czerwiec_2024_Zad4/Matura_Czerwiec_2024_Zad4.cpp
+++ Matura_Czerwiec_2024_Zad4/Matura_Czerwiec_2024_Zad4.cpp
@@ -13,9 +13,9 @@ using namespace std;
 int main()
 {
     //fstream plik("odbiorcy_przyklad.txt");
-    //int n = 16;
+    //int const N = 16;
     fstream plik("odbiorcy.txt");
-    int n = 1024;
+    int const N = 1024;
     
     cout << "Zad 4.2" << endl;
     vector <int> odbiorcy;
@@ -25,12 +25,36 @@ int main()
         odbiorcy.push_back(stoi(linia));
     }
     int licz = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= N; i++) {
         if (find(odbiorcy.begin(), odbiorcy.end(), i) == odbiorcy.end()) {
             licz++;
         }
     }
     cout << licz << endl;
+    plik.clear();
+    plik.seekg(0);
 
     cout << "Zad 4.3" << endl;
+    
+    int pakiet = N;
+
+    int przesylanie[101][N+1];
+
+    for (int pakiet = 1; pakiet <= N; pakiet++) {
+        przesylanie[1][pakiet] = odbiorcy[pakiet - 1];
+
+    }
+
+     for (int runda = 2; runda <= 100; runda++) {
+        for (int pakiet = 1; pakiet <= N; pakiet++) {
+            przesylanie[runda][pakiet] = odbiorcy[przesylanie[runda - 1][pakiet]-1];
+            if (przesylanie[runda][pakiet] == pakiet){
+                cout << runda << " " << pakiet << endl;
+                return 0;
+            }
+
+        }
+    }
+    
+    cout << "Zad 4.4" << endl;
 }
